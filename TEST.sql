ea2110d4051283b8dfbee6e37147dd113f65dccd
jns1appDeveloper <53043874+jns1appDeveloper@users.noreply.github.com>
2024-05-23 13:21:42 +0100

    Version 1.2
    Added foreign key constraint for test table.

--- TEST.sql
+++ TEST.sql
@@ -4,5 +4,7 @@ test_id INT PRIMARY KEY,
 test_name VARCHAR(50),
 test_description TEXT,
 test_date DATETIME
-test_item VARCHAR(20)
+test_item VARCHAR(20),
+data_item INT,
+FOREIGN KEY (data_item) REFERENCES code(code_id)
 );
